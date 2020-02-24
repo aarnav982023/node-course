9cde4dcb35c0ebce900040962c0a74806c4e1b56
aarnav982023 <aarnav982023@gmail.com>
2020-02-25 01:31:19 +0530

    Added task-manager api

---
+++ task-manager/mongodb.js
@@ -0,0 +1,28 @@
+//CRUD create read update delete
+
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb");
+
+const connectionUrl = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+    const db = client.db(databaseName);
+
+    db.collection("tasks")
+      .deleteOne({
+        description: "Learn node express"
+      })
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+  }
+);
