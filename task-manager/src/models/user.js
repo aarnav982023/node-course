59c9b1d0f3270c3e43944a13af9f11caae79c8fa
aarnav982023 <aarnav982023@gmail.com>
2020-03-03 23:33:09 +0530

    Added image upload to task-manager

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -45,7 +45,10 @@ const userSchema = new mongoose.Schema(
           required: true
         }
       }
-    ]
+    ],
+    avatar: {
+      type: Buffer
+    }
   },
   {
     timestamps: true
@@ -79,6 +82,7 @@ userSchema.methods.toJSON = function() {
   const userObject = user.toObject();
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
   return userObject;
 };
 
